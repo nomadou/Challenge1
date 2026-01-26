b7d6ea95182e78960ff486420103c6924285774e
ouyenting <andy.ou@innonet.com.tw>
2026-01-26 21:40:52 +0800

    Implement function of Exchange Hands

--- Showdown/AIPlayer.cs
+++ Showdown/AIPlayer.cs
@@ -2,6 +2,8 @@ namespace Showdown;
 
 public class AIPlayer : Player
 {
+    private Random random = new Random();
+
     public AIPlayer(string defaultName) : base(defaultName) {}
 
     public void DisplayDefaultName()
@@ -11,10 +13,31 @@ public class AIPlayer : Player
 
     public override Card PlayCard()
     {
-        Random random = new Random();
+        if (HandCount == 0)
+        {
+            return null!;
+        }
+        
         int cardIndex = random.Next(HandCount);
         Card card = GetCard(cardIndex);
         RemoveCard(cardIndex);
         return card;
+    }
+
+    public override bool WantToExchange()
+    {
+        if (HasExchanged)
+        {
+            return false;
+        }
+
+        // AI 有 30% 的概率選擇換牌
+        return random.Next(100) < 30;
+    }
+
+    public int SelectPlayerToExchange(List<Player> otherPlayers)
+    {
+        // AI 隨機選擇一個玩家
+        return random.Next(otherPlayers.Count);
     }
 }
